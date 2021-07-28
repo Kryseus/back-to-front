0a003ee59bb1c13de7a4862fddcde5f091b94a26
Philip Tronser <tronser@web.de>
2021-07-28 11:52:54 +0200

    Jorge kickstart

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const MyCard = () => {
+  return (
+    <Card>
+      <Card.Body>This is some text within a card body.</Card.Body>
+    </Card>
+  );
+};
+
+export default MyCard;
